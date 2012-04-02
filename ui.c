4c3a254ccec1541df74e19b3779ffb50b8667eaa
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-02 10:51:01 +0300

    some fixes to get touch recovery working on international sgs2

--- ui.c
+++ ui.c
@@ -474,12 +474,12 @@ static int input_callback(int fd, short revents, void *data)
         rel_sum = 0;
     }
 
-    if (ev.type == 3 && ev.code == 48 && ev.value != 0) {
+    if (ev.type == 3 && ev.code == 57 && ev.value != -1) {
         if (in_touch == 0) {
             in_touch = 1; //starting to track touch...
             reset_gestures();
         }
-    } else if (ev.type == 3 && ev.code == 48 && ev.value == 0) {
+    } else if (ev.type == 3 && ev.code == 57 && ev.value == -1) {
             //finger lifted! lets run with this
             ev.type = EV_KEY; //touch panel support!!!
             int keywidth = gr_get_width(surface) / 4;
@@ -500,13 +500,13 @@ static int input_callback(int fd, short revents, void *data)
                     reset_gestures();
                 } else {
                     //enter key
-                    ev.code = KEY_ENTER;
+                    ev.code = KEY_POWER;
                     reset_gestures();
                 }
                 vibrate(VIBRATOR_TIME_MS);
             }
             if (slide_right == 1) {
-                ev.code = KEY_ENTER;
+                ev.code = KEY_POWER;
                 slide_right = 0;
             } else if (slide_left == 1) {
                 ev.code = KEY_BACK;
@@ -1005,7 +1005,7 @@ int input_buttons()
         end_draw = (keywidth * 3) + keyoffset;
     } else if (touch_x < ((keywidth * 4) + keyoffset + 1)) {
         //enter key
-        final_code = KEY_ENTER;
+        final_code = KEY_POWER;
         start_draw = (keywidth * 3) + keyoffset + 1;
         end_draw = (keywidth * 4) + keyoffset;
     }
